773a9cb56eae9342c3ad62b553777ef9ebe93c43
Fakhry-alta <fakhry.ikhsan.firdaus@gmail.com>
2022-06-03 18:37:37 +0700

    add notes for db prepare statement

--- 9-raw-sql-go/project-structured/controllers/mahasiswa/mahasiswa.go
+++ 9-raw-sql-go/project-structured/controllers/mahasiswa/mahasiswa.go
@@ -27,6 +27,12 @@ func GetAllMahasiswa(db *sql.DB) []_entities.Mahasiswa {
 	return dataAll
 }
 
+/*
+Dalam fungsi Create ini saya menggunakan pendekatan prepare statement. untuk meningkatkan performa aplikasi saat akses ke DB.
+untuk detail penjelasan terkait prepare statement bisa dibaca di link berikut:
+https://medium.com/pujanggateknologi/prepared-statement-di-go-927b1a8863ec
+*/
+
 func CreateMahasiswa(db *sql.DB, newMahasiswa _entities.Mahasiswa) (int, error) {
 	// var query = fmt.Sprintf("INSERT INTO mahasiswa (id, nama, jenis_kelamin, alamat, jurusan_id, telp, status) values (%d, '%s', '%s', '%s', %d, '%s','%s')", newMahasiswa.ID, newMahasiswa.Nama, newMahasiswa.JenisKelamin, newMahasiswa.Alamat, newMahasiswa.JurusanId, newMahasiswa.Telp, newMahasiswa.Status.String)
 	var query = (`INSERT INTO mahasiswa (id, nama, jenis_kelamin, alamat, jurusan_id, telp, status) VALUES (?, ?, ?, ?, ?, ?, ?)`)
