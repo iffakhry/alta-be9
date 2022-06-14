4e109aa6a1eedaae35efc7ad50ce591dc5a8ea5d
Fakhry-alta <fakhry.ikhsan.firdaus@gmail.com>
2022-06-14 16:45:28 +0700

    add rest-api-part3-1

--- 11-echo-gorm/main.go
+++ 11-echo-gorm/main.go
@@ -19,6 +19,8 @@ type User struct {
 	Name     string `json:"name" form:"name"`
 	Email    string `gorm:"unique" json:"email" form:"email"`
 	Password string `json:"password" form:"password"`
+	Products []Product
+	Address  Address
 }
 
 type Jurusan struct {
@@ -28,6 +30,25 @@ type Jurusan struct {
 	UpdatedAt time.Time      `gorm:"autoCreateTime:false" json:"updated_at" form:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"autoCreateTime:false" json:"deleted_at" form:"deleted_at"`
 	Name      string         `json:"name" form:"name"`
+}
+
+type Product struct {
+	gorm.Model
+	UserID uint   `json:"user_id" form:"user_id"`
+	Name   string `json:"name" form:"name"`
+	Price  int    `json:"price" form:"price"`
+}
+
+type Address struct {
+	gorm.Model
+	UserID  uint   `json:"user_id" form:"user_id"`
+	Address string `json:"address" form:"address"`
+}
+
+type ResponseUserFormat struct {
+	ID    uint   `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
 
 var DB *gorm.DB
@@ -49,6 +70,7 @@ func InitDB() {
 func InitialMigration() {
 	DB.AutoMigrate(&User{})
 	DB.AutoMigrate(&Jurusan{})
+	DB.AutoMigrate(&Product{})
 }
 
 func init() {
@@ -69,20 +91,51 @@ func main() {
 	e.Logger.Fatal(e.Start(":80"))
 }
 
+func ResponseFailed(msg string) map[string]interface{} {
+	return map[string]interface{}{
+		"status":  "error",
+		"message": msg,
+	}
+}
+
+func ResponseSuccessNoData(msg string) map[string]interface{} {
+	return map[string]interface{}{
+		"status":  "success",
+		"message": msg,
+	}
+}
+
+func ResponseSuccessWithData(msg string, data interface{}) map[string]interface{} {
+	return map[string]interface{}{
+		"status":  "success",
+		"message": msg,
+		"data":    data,
+	}
+}
+
 func GetUsersController(c echo.Context) error {
 	var users []User
 	tx := DB.Find(&users)
 	if tx.Error != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
-			"status":  "error",
-			"message": "failed to get data",
-		})
+
+		return c.JSON(http.StatusInternalServerError, ResponseFailed("failed to get data"))
 	}
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"status":  "success",
-		"message": "success get data",
-		"data":    users,
-	})
+	// return c.JSON(http.StatusOK, map[string]interface{}{
+	// 	"status":  "success",
+	// 	"message": "success get data",
+	// 	"data":    users,
+	// })
+
+	responseData := []ResponseUserFormat{}
+	for _, value := range users {
+		var response ResponseUserFormat
+		response.ID = value.ID
+		response.Name = value.Name
+		response.Email = value.Email
+
+		responseData = append(responseData, response)
+	}
+	return c.JSON(http.StatusOK, ResponseSuccessWithData("success get data", responseData))
 }
 
 func CreateUserController(c echo.Context) error {
@@ -90,10 +143,8 @@ func CreateUserController(c echo.Context) error {
 	err := c.Bind(&user)
 	if err != nil {
 		fmt.Println("error", err)
-		return c.JSON(http.StatusBadRequest, map[string]interface{}{
-			"status":  "error",
-			"message": "failed create user bind",
-		})
+
+		return c.JSON(http.StatusBadRequest, ResponseFailed("failed create user bind"))
 	}
 	// gmt, _ := time.LoadLocation("UTC")
 	// time.Local = gmt
@@ -101,29 +152,30 @@ func CreateUserController(c echo.Context) error {
 	// fmt.Println(user.CreatedAt)
 	tx := DB.Create(&user)
 	if tx.Error != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
-			"status":  "error",
-			"message": "failed to insert data",
-		})
+
+		return c.JSON(http.StatusInternalServerError, ResponseFailed("failed to insert data"))
 	}
 	if tx.RowsAffected == 0 {
-		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
-			"status":  "error",
-			"message": "failed to insert data",
-		})
+		// return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+		// 	"status":  "error",
+		// 	"message": "failed to insert data",
+		// })
+		return c.JSON(http.StatusInternalServerError, ResponseFailed("failed to insert data"))
 	}
 	// To change its value, you could use `Update`
 	// DB.Model(&user).Update("created_at", time.Now().In(gmt))
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"status":  "success",
-		"message": "success insert data",
-	})
+
+	return c.JSON(http.StatusOK, ResponseSuccessNoData("success insert data"))
 
 }
 
 func CreateJurusanController(c echo.Context) error {
-	jurusan := Jurusan{}
-	err := c.Bind(&jurusan)
+
+	type jurusanRequestFormat struct {
+		Name string `json:"name" form:"name"`
+	}
+	jurusanReqBody := jurusanRequestFormat{}
+	err := c.Bind(&jurusanReqBody)
 	if err != nil {
 		fmt.Println("error", err)
 		return c.JSON(http.StatusBadRequest, map[string]interface{}{
@@ -136,6 +188,9 @@ func CreateJurusanController(c echo.Context) error {
 	// jurusan.CreatedAt = time.Now().In(gmt)
 	// jurusan.UpdatedAt = time.Now().In(gmt)
 	// fmt.Println(jurusan.CreatedAt)
+	jurusan := Jurusan{
+		Name: jurusanReqBody.Name,
+	}
 	tx := DB.Create(&jurusan)
 	if tx.Error != nil {
 		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
@@ -157,3 +212,9 @@ func CreateJurusanController(c echo.Context) error {
 	})
 
 }
+
+// func UpdateUser(c echo.Context) error {
+// 	user := User{}
+// 	user.ID = 111
+// 	db.Model(&user).Update("name", "hello")
+// }
---
+++ rest-api-part3-1/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var (
+	DB *gorm.DB
+)
+
+func init() {
+	InitDB()
+	InitialMigration()
+}
+
+type Config struct {
+	DB_Username string
+	DB_Password string
+	DB_Port     string
+	DB_Host     string
+	DB_Name     string
+}
+
+func InitDB() {
+
+	config := Config{
+		DB_Username: "root",
+		DB_Password: "root123",
+		DB_Port:     "3306",
+		DB_Host:     "localhost",
+		DB_Name:     "crud_go",
+	}
+
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		config.DB_Username,
+		config.DB_Password,
+		config.DB_Host,
+		config.DB_Port,
+		config.DB_Name,
+	)
+
+	var err error
+	DB, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+type User struct {
+	gorm.Model
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
+}
+
+func InitialMigration() {
+	DB.AutoMigrate(&User{})
+}
+
+// get all users
+func GetUsersController(c echo.Context) error {
+	var users []User
+
+	if err := DB.Find(&users).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success get all users",
+		"users":   users,
+	})
+}
+
+// get user by id
+func GetUserController(c echo.Context) error {
+	// your solution here
+}
+
+// create new user
+func CreateUserController(c echo.Context) error {
+	user := User{}
+	c.Bind(&user)
+
+	if err := DB.Save(&user).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success create new user",
+		"user":    user,
+	})
+}
+
+// delete user by id
+func DeleteUserController(c echo.Context) error {
+	// your solution here
+}
+
+// update user by id
+func UpdateUserController(c echo.Context) error {
+	// your solution here
+}
+
+func main() {
+	// create a new echo instance
+	e := echo.New()
+	// Route / to handler function
+	e.GET("/users", GetUsersController)
+	e.GET("/users/:id", GetUserController)
+	e.POST("/users", CreateUserController)
+	e.DELETE("/users/:id", DeleteUserController)
+	e.PUT("/users/:id", UpdateUserController)
+
+	// start the server, and log if it fails
+	e.Logger.Fatal(e.Start(":8000"))
+}
